aaaf785f21aaf68004390f9151c27dc7f33671f5
David Granados <dgranadosz@aqualogy.net>
2016-06-29 08:35:28 +0200

    Cargada dinamicamente componente pasandole parametros

--- AngularTest/app/app.component.js
+++ AngularTest/app/app.component.js
@@ -12,10 +12,15 @@ var core_1 = require('@angular/core');
 var ChildComponent1 = (function () {
     function ChildComponent1() {
     }
+    ;
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', String)
+    ], ChildComponent1.prototype, "content", void 0);
     ChildComponent1 = __decorate([
         core_1.Component({
             selector: 'compo1',
-            template: '<h1>Este es el componente <b>UNO</b > </h1>'
+            templateUrl: '/app/compo1.html'
         }), 
         __metadata('design:paramtypes', [])
     ], ChildComponent1);
@@ -24,40 +29,72 @@ var ChildComponent1 = (function () {
 var ChildComponent2 = (function () {
     function ChildComponent2() {
     }
+    ;
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', String)
+    ], ChildComponent2.prototype, "content", void 0);
     ChildComponent2 = __decorate([
         core_1.Component({
             selector: 'compo2',
-            template: '<h1>Este es el componente <b>DOS</b > </h1>'
+            templateUrl: '/app/compo2.html'
         }), 
         __metadata('design:paramtypes', [])
     ], ChildComponent2);
     return ChildComponent2;
 }());
 var AppComponent = (function () {
-    function AppComponent(dcl, injector) {
-        this.dcl = dcl;
-        this.injector = injector;
+    function AppComponent(componentResolver) {
+        this.componentResolver = componentResolver;
         this.v = 0;
+        this.isViewInitialized = false;
     }
-    AppComponent.prototype.ngOnInit = function () {
+    AppComponent.prototype.updateComponent = function () {
         var _this = this;
         setInterval(function () {
+            if (!_this.isViewInitialized) {
+                return;
+            }
+            if (_this.cmpRef) {
+                _this.cmpRef.destroy();
+            }
             if (_this.v % 2 == 0) {
-                _this.dcl.loadAsRoot(ChildComponent1, '#child', _this.injector);
+                _this.componentResolver.resolveComponent(ChildComponent1).then(function (factory) {
+                    _this.cmpRef = _this.target.createComponent(factory);
+                    _this.cmpRef.instance.content = "UNO!!";
+                });
             }
             else {
-                _this.dcl.loadAsRoot(ChildComponent2, '#child', _this.injector);
+                _this.componentResolver.resolveComponent(ChildComponent2).then(function (factory) {
+                    _this.cmpRef = _this.target.createComponent(factory);
+                    _this.cmpRef.instance.content = "DOS!!";
+                });
             }
             _this.v++;
-            console.log(_this.v);
         }, 1000);
     };
+    AppComponent.prototype.ngOnChanges = function () {
+        this.updateComponent();
+    };
+    AppComponent.prototype.ngAfterViewInit = function () {
+        this.isViewInitialized = true;
+        this.updateComponent();
+    };
+    AppComponent.prototype.ngOnDestroy = function () {
+        if (this.cmpRef) {
+            this.cmpRef.destroy();
+        }
+    };
+    __decorate([
+        core_1.ViewChild('target', { read: core_1.ViewContainerRef }), 
+        __metadata('design:type', Object)
+    ], AppComponent.prototype, "target", void 0);
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<div id="child"></div>'
+            template: '<div #target></div>'
         }), 
-        __metadata('design:paramtypes', [core_1.DynamicComponentLoader, core_1.Injector])
+        __metadata('design:paramtypes', [core_1.ComponentResolver])
     ], AppComponent);
     return AppComponent;
 }());
--- AngularTest/app/app.component.ts
+++ AngularTest/app/app.component.ts
@@ -1,40 +1,80 @@
 ﻿
-import { Component, Inject, DynamicComponentLoader, ViewContainerRef, OnInit, Injector} from '@angular/core';
+import {Component, ComponentRef, Input, ViewContainerRef, ComponentFactory, ComponentResolver, ViewChild, Injectable} from '@angular/core';
 
+export interface IComponent {
+    content: string;
+}
 
 @Component({
     selector: 'compo1',
-    template: '<h1>Este es el componente <b>UNO</b > </h1>'
+    templateUrl: '/app/compo1.html'
 })
-class ChildComponent1 { }
+class ChildComponent1 {
+    @Input() content: string;
+
+    constructor() { };
+}
 
 @Component({
     selector: 'compo2',
-    template: '<h1>Este es el componente <b>DOS</b > </h1>'
+    templateUrl: '/app/compo2.html'
 })
-class ChildComponent2 { }
+class ChildComponent2 {
+    @Input() content: string;
+
+    constructor() { };
+}
 
 @Component({
     selector: 'my-app',
-    template: '<div id="child"></div>'
+    template: '<div #target></div>'
 })
 
-export class AppComponent implements OnInit {
+
+export class AppComponent{
+    @ViewChild('target', { read: ViewContainerRef }) target;
     private v: number = 0;
+    cmpRef: ComponentRef<IComponent>;
+    private isViewInitialized: boolean = false;
+    constructor(private componentResolver: ComponentResolver ) { }
 
-    constructor(private dcl: DynamicComponentLoader, private injector: Injector) { }
-
-    ngOnInit() {
+    updateComponent() {
         setInterval(() => {
+            if (!this.isViewInitialized) {
+                return;
+            }
+            if (this.cmpRef) {
+                this.cmpRef.destroy();
+            }
             if (this.v % 2 == 0) {
-
-                this.dcl.loadAsRoot(ChildComponent1, '#child', this.injector);
+                this.componentResolver.resolveComponent(ChildComponent1).then(factory => {
+                    this.cmpRef = this.target.createComponent(factory);
+                    this.cmpRef.instance.content = "UNO!!";
+                });
             }
             else {
-                this.dcl.loadAsRoot(ChildComponent2, '#child', this.injector);
+                this.componentResolver.resolveComponent(ChildComponent2).then(factory => {
+                    this.cmpRef = this.target.createComponent(factory);
+                    this.cmpRef.instance.content = "DOS!!";
+                });
             }
+            
             this.v++;           
 
         }, 1000);
+    }
+    ngOnChanges() {
+        this.updateComponent();
+    }
+
+    ngAfterViewInit() {
+        this.isViewInitialized = true;
+        this.updateComponent();
+    }
+
+    ngOnDestroy() {
+        if (this.cmpRef) {
+            this.cmpRef.destroy();
+        }
     }
 }
